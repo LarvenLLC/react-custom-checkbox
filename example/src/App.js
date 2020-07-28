f1b76e1d135327bf7ab72d1135fb6da9b9955ca5
BossBele <adambeleko@gmail.com>
2020-07-28 20:59:02 +0300

    updated docs

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
-import Checkbox from 'react-custom-checkbox'
+import React from "react";
+import * as Icon from "react-icons/fi";
+import Checkbox from "react-custom-checkbox";
 
 const App = () => {
-  return <Checkbox />
-}
+  return (
+    <>
+      <h4>Default</h4>
+      <Checkbox />
+      <h4>Using Custom Icon</h4>
+      <Checkbox
+        icon={<Icon.FiCheck color="#174A41" size={14} />}
+        name="my-input"
+        checked={true}
+        onChange={(value) => {
+          let p = {
+            isTrue: value,
+          };
+          return alert(value);
+        }}
+        borderColor="#D7C629"
+        style={{ cursor: "pointer" }}
+        labelStyle={{ userSelect: "none" }}
+        label="Have you started using it?"
+      />
+    </>
+  );
+};
 
-export default App
+export default App;
