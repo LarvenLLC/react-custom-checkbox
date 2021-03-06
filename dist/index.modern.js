2f81487e611c4a7e02640063767ae9fef8c53c38
BossBele <adambeleko@gmail.com>
2021-03-06 10:07:22 +0300

    publish

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,73 +1,52 @@
 import React, { useState, useEffect } from 'react';
 
-function _extends() {
-  _extends = Object.assign || function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
+const Checkbox = props => {
+  const {
+    borderColor,
+    borderRadius,
+    borderStyle,
+    borderWidth,
+    checkbox,
+    className,
+    checked,
+    disabled,
+    containerClassName,
+    containerStyle,
+    label,
+    labelClassName,
+    labelStyle,
+    name,
+    onChange,
+    reference,
+    right,
+    size,
+    style,
+    value,
+    icon
+  } = props;
+  const [check, setCheck] = useState(checked);
 
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-
-    return target;
-  };
-
-  return _extends.apply(this, arguments);
-}
-
-var Checkbox = function Checkbox(props) {
-  var borderColor = props.borderColor,
-      borderRadius = props.borderRadius,
-      borderStyle = props.borderStyle,
-      borderWidth = props.borderWidth,
-      checkbox = props.checkbox,
-      className = props.className,
-      checked = props.checked,
-      disabled = props.disabled,
-      containerClassName = props.containerClassName,
-      containerStyle = props.containerStyle,
-      label = props.label,
-      labelClassName = props.labelClassName,
-      labelStyle = props.labelStyle,
-      name = props.name,
-      onChange = props.onChange,
-      reference = props.reference,
-      right = props.right,
-      size = props.size,
-      style = props.style,
-      value = props.value,
-      icon = props.icon;
-
-  var _useState = useState(checked),
-      check = _useState[0],
-      setCheck = _useState[1];
-
-  var toggle = function toggle(e) {
+  const toggle = e => {
     e.preventDefault();
     setCheck(!check);
     onChange && onChange(!check);
   };
 
-  useEffect(function () {
+  useEffect(() => {
     setCheck(checked);
   }, [checked]);
   return /*#__PURE__*/React.createElement("label", {
-    style: _extends({}, containerStyle, {
+    style: { ...containerStyle,
       display: "flex",
       alignItems: "center"
-    }),
+    },
     className: containerClassName,
-    onClick: function onClick(e) {
-      return !disabled ? toggle(e) : null;
-    }
+    onClick: e => !disabled ? toggle(e) : null
   }, right && label && /*#__PURE__*/React.createElement("span", {
     className: labelClassName,
     style: labelStyle
   }, label) || null, checkbox || /*#__PURE__*/React.createElement("span", null, /*#__PURE__*/React.createElement("div", {
-    style: _extends({}, style, {
+    style: { ...style,
       height: size,
       width: size,
       borderWidth: borderWidth,
@@ -78,7 +57,7 @@ var Checkbox = function Checkbox(props) {
       alignItems: "center",
       justifyContent: "center",
       cursor: disabled ? "not-allowed" : ""
-    }),
+    },
     className: className
   }, check && icon || null, /*#__PURE__*/React.createElement("input", {
     ref: reference,
