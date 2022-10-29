40d9765f2e7bde0e6092d2942632f4a9fa9b9bb7
BossBele <adambeleko@gmail.com>
2022-10-29 10:23:22 +0300

    upgrade to react 18

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,70 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-function _extends() {
-  _extends = Object.assign || function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
+const Checkbox = props => {
+  const {
+    borderColor,
+    borderRadius,
+    borderStyle,
+    borderWidth,
+    checkbox,
+    className,
+    checked,
+    disabled,
+    containerClassName,
+    containerStyle,
+    label,
+    labelClassName,
+    labelStyle,
+    name,
+    onChange,
+    reference,
+    right,
+    size,
+    style,
+    value,
+    icon,
+    tabIndex,
+    ...rest
+  } = props;
+  const [check, setCheck] = useState(checked);
 
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-
-    return target;
-  };
-
-  return _extends.apply(this, arguments);
-}
-
-function _objectWithoutPropertiesLoose(source, excluded) {
-  if (source == null) return {};
-  var target = {};
-  var sourceKeys = Object.keys(source);
-  var key, i;
-
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
-    if (excluded.indexOf(key) >= 0) continue;
-    target[key] = source[key];
-  }
-
-  return target;
-}
-
-var _excluded = ["borderColor", "borderRadius", "borderStyle", "borderWidth", "checkbox", "className", "checked", "disabled", "containerClassName", "containerStyle", "label", "labelClassName", "labelStyle", "name", "onChange", "reference", "right", "size", "style", "value", "icon", "tabIndex"];
-
-var Checkbox = function Checkbox(props) {
-  var borderColor = props.borderColor,
-      borderRadius = props.borderRadius,
-      borderStyle = props.borderStyle,
-      borderWidth = props.borderWidth,
-      checkbox = props.checkbox,
-      className = props.className,
-      checked = props.checked,
-      disabled = props.disabled,
-      containerClassName = props.containerClassName,
-      containerStyle = props.containerStyle,
-      label = props.label,
-      labelClassName = props.labelClassName,
-      labelStyle = props.labelStyle,
-      name = props.name,
-      onChange = props.onChange,
-      reference = props.reference,
-      right = props.right,
-      size = props.size,
-      style = props.style,
-      value = props.value,
-      icon = props.icon,
-      tabIndex = props.tabIndex,
-      rest = _objectWithoutPropertiesLoose(props, _excluded);
-
-  var _useState = useState(checked),
-      check = _useState[0],
-      setCheck = _useState[1];
-
-  var toggle = function toggle(e) {
+  const toggle = e => {
     e.preventDefault();
 
     if (disabled) {
@@ -75,43 +39,42 @@ var Checkbox = function Checkbox(props) {
     onChange && onChange(!check, e);
   };
 
-  useEffect(function () {
+  useEffect(() => {
     setCheck(checked);
   }, [checked]);
   return /*#__PURE__*/React.createElement("label", {
-    style: _extends({
+    style: {
       display: "flex",
-      alignItems: "center"
-    }, containerStyle),
+      alignItems: "center",
+      ...containerStyle
+    },
     className: containerClassName,
-    onClick: function onClick(e) {
-      return toggle(e);
-    }
+    onClick: e => toggle(e)
   }, right && label && /*#__PURE__*/React.createElement("span", {
     className: labelClassName,
     style: labelStyle
   }, label) || null, checkbox || /*#__PURE__*/React.createElement("span", null, /*#__PURE__*/React.createElement("div", {
-    style: _extends({
+    style: {
       height: size,
       width: size,
       borderWidth: borderWidth,
       borderColor: borderColor,
       borderStyle: borderStyle,
-      borderRadius: borderRadius
-    }, style, {
+      borderRadius: borderRadius,
+      ...style,
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
       cursor: disabled ? "not-allowed" : ""
-    }),
+    },
     tabIndex: tabIndex,
-    onKeyDown: function onKeyDown(e) {
+    onKeyDown: e => {
       if (e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar') {
         toggle(e);
       }
     },
     className: className
-  }, check && icon || null, /*#__PURE__*/React.createElement("input", _extends({}, rest, {
+  }, check && icon || null, /*#__PURE__*/React.createElement("input", Object.assign({}, rest, {
     ref: reference,
     type: "checkbox",
     name: name,
