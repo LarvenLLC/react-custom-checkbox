90a41703f250a41fbc1c44a0b56a6f5b3d554906
BossBele <adambeleko@gmail.com>
2022-11-07 21:57:56 +0300

    added switch api

--- switch/index.modern.js
+++ switch/index.modern.js
@@ -1,71 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-function _extends() {
-  _extends = Object.assign || function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-
-    return target;
-  };
-
-  return _extends.apply(this, arguments);
-}
-
-function _objectWithoutPropertiesLoose(source, excluded) {
-  if (source == null) return {};
-  var target = {};
-  var sourceKeys = Object.keys(source);
-  var key, i;
-
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
-    if (excluded.indexOf(key) >= 0) continue;
-    target[key] = source[key];
-  }
-
-  return target;
-}
-
-var _excluded = ["iconClassName", "iconStyle", "checked", "checkedIconStyle", "checkedIndicatorStyle", "checkedTrackStyle", "className", "containerClassName", "containerStyle", "disabled", "icon", "id", "indicatorClassName", "indicatorStyle", "labelStyle", "labelClassName", "name", "onChange", "reference", "style", "trackClassName", "trackStyle", "value"];
-
 function Switch(props) {
-  var iconClassName = props.iconClassName,
-      iconStyle = props.iconStyle,
-      checked = props.checked,
-      checkedIconStyle = props.checkedIconStyle,
-      checkedIndicatorStyle = props.checkedIndicatorStyle,
-      checkedTrackStyle = props.checkedTrackStyle,
-      className = props.className,
-      containerClassName = props.containerClassName,
-      containerStyle = props.containerStyle,
-      disabled = props.disabled,
-      icon = props.icon,
-      id = props.id,
-      indicatorClassName = props.indicatorClassName,
-      indicatorStyle = props.indicatorStyle,
-      labelStyle = props.labelStyle,
-      labelClassName = props.labelClassName,
-      name = props.name,
-      onChange = props.onChange,
-      reference = props.reference,
-      style = props.style,
-      trackClassName = props.trackClassName,
-      trackStyle = props.trackStyle,
-      value = props.value,
-      rest = _objectWithoutPropertiesLoose(props, _excluded);
+  const {
+    iconClassName,
+    iconStyle,
+    checked,
+    checkedIconStyle,
+    checkedIndicatorStyle,
+    checkedTrackStyle,
+    className,
+    containerClassName,
+    containerStyle,
+    disabled,
+    icon,
+    indicatorClassName,
+    indicatorStyle,
+    labelStyle,
+    labelClassName,
+    name,
+    onChange,
+    reference,
+    style,
+    trackClassName,
+    trackStyle,
+    value,
+    ...rest
+  } = props;
+  const [check, setCheck] = useState(checked);
 
-  var _useState = useState(checked),
-      check = _useState[0],
-      setCheck = _useState[1];
-
-  var toggle = function toggle(e) {
+  const toggle = e => {
     e.preventDefault();
 
     if (disabled) {
@@ -76,7 +39,7 @@ function Switch(props) {
     onChange && onChange(!check, e);
   };
 
-  useEffect(function () {
+  useEffect(() => {
     setCheck(checked);
   }, [checked]);
   return /*#__PURE__*/React.createElement("div", {
@@ -85,29 +48,33 @@ function Switch(props) {
   }, /*#__PURE__*/React.createElement("label", {
     className: labelClassName,
     style: labelStyle,
-    htmlFor: id,
     onClick: toggle
-  }, /*#__PURE__*/React.createElement("input", _extends({}, rest, {
+  }, /*#__PURE__*/React.createElement("input", Object.assign({}, rest, {
     ref: reference,
     name: name,
     type: "checkbox",
     className: className,
     style: style,
-    checked: checked,
-    id: id,
+    checked: check,
     value: value,
     onChange: toggle,
     disabled: disabled,
     hidden: true
   })), /*#__PURE__*/React.createElement("span", {
     className: trackClassName,
-    style: _extends({}, trackStyle, checked ? checkedTrackStyle : {})
+    style: { ...trackStyle,
+      ...(check ? checkedTrackStyle : {})
+    }
   }, /*#__PURE__*/React.createElement("span", {
     className: indicatorClassName,
-    style: _extends({}, indicatorStyle, checked ? checkedIndicatorStyle : {})
+    style: { ...indicatorStyle,
+      ...(check ? checkedIndicatorStyle : {})
+    }
   }, icon ? /*#__PURE__*/React.createElement("span", {
     className: iconClassName,
-    style: _extends({}, iconStyle, checked ? checkedIconStyle : {})
+    style: { ...iconStyle,
+      ...(check ? checkedIconStyle : {})
+    }
   }, icon) : null))));
 }
 
@@ -120,14 +87,24 @@ Switch.defaultProps = {
     opacity: 0,
     transition: 'all 0.25s ease-in-out'
   },
-  checkedIconStyle: {},
-  checkedIndicatorStyle: {},
-  checkedTrackStyle: {},
+  checkedIconStyle: {
+    opacity: 1,
+    transition: 'all 0.25s ease-in-out'
+  },
+  checkedIndicatorStyle: {
+    background: '#121943',
+    transform: 'translateX(30px)'
+  },
+  checkedTrackStyle: {
+    border: '1px solid transparent',
+    boxShadow: '0px 0px 0px 2px #121943',
+    opacity: 1,
+    transition: 'all 0.25s ease-in-out'
+  },
   className: "larven__toggleInput",
   containerStyle: {
     display: 'inline-block'
   },
-  id: "larven__switchID",
   indicatorClassName: "larven__toggle-indicator",
   indicatorStyle: {
     alignItems: 'center',
@@ -171,15 +148,7 @@ Switch.defaultProps = {
     marginRight: 12,
     position: 'relative',
     width: 60
-  },
-  icon: /*#__PURE__*/React.createElement("svg", {
-    viewBox: "0 0 24 24",
-    id: "ghq-svg-check",
-    role: "presentation",
-    "aria-hidden": "true"
-  }, /*#__PURE__*/React.createElement("path", {
-    d: "M9.86 18a1 1 0 01-.73-.32l-4.86-5.17a1.001 1.001 0 011.46-1.37l4.12 4.39 8.41-9.2a1 1 0 111.48 1.34l-9.14 10a1 1 0 01-.73.33h-.01z"
-  }))
+  }
 };
 
 export default Switch;
