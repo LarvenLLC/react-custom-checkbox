9f2521a21d4f5f8d4fb5d8c2a8d09d5ce2de9fb6
BossBele <adambeleko@gmail.com>
2023-01-15 14:50:56 +0300

    add library types

--- index.d.ts
+++ index.d.ts
@@ -2,29 +2,49 @@
 // Project: https://github.com/LarvenLLC/react-custom-checkbox
 
 interface CheckboxProps {
+  /** Custom check icon */
   icon: string;
+  /** State of checkbox */
   checked: boolean;
+  /** Checkbox input active/inactive state */
   disabled: boolean;
+  /** Checkbox label text */
   label: string;
+  /** Function triggered when checked state changes */
   onChange(checked: boolean, event: React.ChangeEvent<HTMLInputElement>): string;
+  /** Size of checkbox */
   size: number;
+  /** Label position right? */
   right: boolean;
+  /** Checkbox input name */
   name: string;
+  /** Checkbox input value */
   value: string;
+  /** Checkbox input ref */
   reference: React.RefObject<HTMLInputElement>;
+  /** Checkbox css style */
   style: React.CSSProperties;
+  /** Checkbox css class(es) */
   className: string;
+  /** Color of checkbox border */
   borderColor: string;
+  /** Radius of checkbox border */
   borderRadius: number;
+  /** Style of checkbox border */
   borderStyle: React.CSSProperties;
+  /** Thickness of checkbox border */
   borderWidth: number;
+  /** Label text css class(es) */
   labelClassName: string;
+  /** Label text css style */
   labelStyle: React.CSSProperties;
+  /** Checkbox & label container css class(es) */
   containerClassName: string;
+  /** Checkbox & label container css style */
   containerStyle: React.CSSProperties;
 }
 
-declare function Checkbox(props?): React.FC<CheckboxProps>;
+declare const Checkbox: React.FC<CheckboxProps>;
 
 /**
  * Usage: <code>import Checkbox from 'react-custom-checkbox'</code>
